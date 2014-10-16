33fddf63626b6f878bedcce7d2901c13af8bf0ea
Fabian Vogelsteller <fabian@frozeman.de>
2014-10-16 11:34:24 +0200

    Update package.js

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "frozeman:animation-helper-velocity",
     summary: "Animates elements inside a {{> AnimateWithVelocity}} block by adding attributes.",
-    version: "0.1.6",
+    version: "0.1.7",
     git: "https://github.com/frozeman/meteor-animation-helper-velocity.git"
 });
 
